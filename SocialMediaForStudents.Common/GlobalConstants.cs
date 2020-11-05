0d3b286c9781fcdb03d1c0b4681fe49277b052e9
elenakarabeteva <e.karabeteva6@gmail.com>
2020-11-05 11:51:22 +0200

    Template changes from the start

---
+++ SocialMediaForStudents.Common/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace SocialMediaForStudents.Common
+{
+    public static class GlobalConstants
+    {
+        public const string SystemName = "SocialMediaForStudents";
+
+        public const string AdministratorRoleName = "Administrator";
+    }
+}
